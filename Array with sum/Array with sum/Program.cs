232dc6a45a6b8797a72045ab57ff854495de95bd
Rivianwolf <giorgi.matiashvili@outlook.com>
2023-06-28 15:22:45 +0400

    New Push

---
+++ Array with sum/Array with sum/Program.cs
@@ -0,0 +1,25 @@
+﻿    using System;
+    class arraySizeCounter
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arraySize = new int[size];
+            Console.WriteLine("Enter elements of the array:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Element {0}:", i + 1);
+                arraySize[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        int sum = 0;
+        foreach (int item in arraySize)
+        {
+            sum += item;
+        }
+        Console.WriteLine("Total: " + sum);
+            
+        }
+    }
+
+
