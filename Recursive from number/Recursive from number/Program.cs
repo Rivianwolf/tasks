2f2a1127bd38db18664f0bb959c9214fc2102a72
Rivianwolf <giorgi.matiashvili@outlook.com>
2023-07-13 18:14:50 +0400

    რეკურსია და ჯანდაბა

---
+++ Recursive from number/Recursive from number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        PrintNumbers(number);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNumbers(int number)
+    {
+        if (number >= 1)
+        {
+            Console.WriteLine(number);
+            PrintNumbers(number - 1);
+        }
+    }
+}
