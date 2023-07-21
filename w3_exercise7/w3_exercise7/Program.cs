0104bdb6c6360385742430b2798ae78d11e97900
Rivianwolf <giorgi.matiashvili@outlook.com>
2023-07-21 17:49:15 +0400

    w3_exercise8

---
+++ w3_exercise7/w3_exercise7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class exercise7
+{
+    public static void Main()  
+    { 
+        Console.WriteLine("Please enter num1:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter num2:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+        Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+        Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1%num2);
+    }
+}
