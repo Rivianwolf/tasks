4ad3373f8080c48bfd89b81c33fa9c661262598e
Rivianwolf <giorgi.matiashvili@outlook.com>
2023-07-26 14:39:03 +0400

    w3_exercise11

---
+++ w3_exercise11/w3_exercise11/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Exercise9
+{
+    public static void Main()
+    {
+        double age;
+        Console.WriteLine("Please enter your age:");
+        age = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("You look younger than {0}", age);
+
+    }
+}
+
+
