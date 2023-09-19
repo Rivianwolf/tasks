69b2d02c84a032616d20d43eecdd88104ce7b8d4
Rivianwolf <giorgi.matiashvili@outlook.com>
2023-09-19 16:04:33 +0400

    test

--- HR/HR/Startup.cs
+++ HR/HR/Startup.cs
@@ -32,7 +32,6 @@ namespace Customers
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen();
         }
 
 
@@ -40,11 +39,6 @@ namespace Customers
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customers");
-            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
